66c28332cfa1e0af98623e899c9c72e7d0ce8d16
kyle molidor <k_molidor@yahoo.com>
2020-02-17 04:53:37 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -8,4 +8,10 @@ def countdown(seconds)
 end
 
 def countdown_with_sleep(seconds)
-  while seconds  
+  while seconds > 0 
+    puts "#{seconds} SECOND(S)!"
+    seconds -= 1
+    
+    (sleeps)
+  end
+end
